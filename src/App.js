af46cb871f79f410bd052be4885fed1d1fb73d5b
Hristo Kisimov <hkisimov@sidigitalsolutions.com>
2021-11-20 11:57:39 +0200

    Remove initial unnecessary code and files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Let's code this tinpet :)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Tinpet
+      </h1>
     </div>
   );
 }
