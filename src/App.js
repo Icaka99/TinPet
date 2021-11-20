d97903d1e42ae326e91da934e34d42901bc9cb3a
Hristo Kisimov <hkisimov@sidigitalsolutions.com>
2021-11-20 11:07:47 +0200

    Add react gitignore file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let's code this tinpet :)
         </p>
         <a
           className="App-link"
