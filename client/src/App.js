40de5232ecd823d3e51254c98ed9480284774c35
Hristo Kisimov <hkisimov@sidigitalsolutions.com>
2021-12-18 20:47:46 +0200

    Extract client in seperate folder

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>
+        Tinpet
+      </h1>
+      <nav className="navigation">
+        <ul>
+          <li className="list-item">
+            <img src="TinPetLogo.png" alt="TinPetLogo"></img>
+          </li>
+          <li className="list-item">
+            <a href="#">Navigation 1</a>
+          </li>
+          <li className="list-item">
+            <a href="#">Navigation 2</a>
+          </li>
+          <li className="list-item">
+            <a href="#">Navigation 3</a>
+          </li>
+          <li className="list-item">
+            <a href="#">Navigation 4</a>
+          </li>
+          <li className="list-item">
+            <a href="#">Navigation 5</a>
+          </li>
+          <li className="list-item">
+            <a href="#">Navigation 6</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
