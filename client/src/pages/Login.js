3dbb1617ae03d8cb6f3547ec1dfa45b95bd7deac
Hristo Kisimov <hkisimov@sidigitalsolutions.com>
2021-12-20 17:06:48 +0200

    Implement logic for Login Change favicon

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,135 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core';
+import { PropTypes } from 'prop-types';
+import AppIcon from '../images/favicon.png';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+//MUI stuff
+import { Grid } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { CircularProgress } from '@material-ui/core';
+
+const styles = {
+    form: {
+        textAlign: 'center'
+    },
+    image: {
+        margin: '20px auto 20px auto',
+        width: '70px',
+        height: '70px',
+    },
+    pageTitle: {
+        margin: '10px auto 10px auto'
+    },
+    textField: {
+        margin: '10px auto 10px auto'
+    },
+    button: {
+        marginTop: 20,
+        position: 'relative'
+    },
+    customError: {
+        color: 'red',
+        fontSize: '0.8rem',
+        marginTop: 10
+    },
+    progress: {
+        position: 'absolute'
+    }
+}
 
 class Login extends Component {
-    render(){
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+            errors: {}
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            loading: true
+        });
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        };
+        axios
+            .post('/login', userData)
+            .then((res) => {
+                this.setState({
+                    loading: false
+                });
+                console.log(this.context);
+                this.props.history.push('/');
+            })
+            .catch((err) => {
+                console.log(err);
+                this.setState({
+                    errors: err.response.data,
+                    loading: false
+                });
+            });
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    render() {
+        const { classes } = this.props;
+        const { errors, loading } = this.state;
         return (
-            <div>
-                Login page
-            </div>
+            <Grid container className={classes.form}>
+                <Grid item sm />
+                <Grid item sm >
+                    <img src={AppIcon} alt="pets" className={classes.image} />
+                    <Typography variant="h3" className={classes.pageTitle}>
+                        Login
+                    </Typography>
+                    <form noValidate onSubmit={this.handleSubmit}>
+                        <TextField
+                            id="email" name="email" type="email" label="Email" fullWidth 
+                            helperText={errors.email} error={errors.email ? true : false}
+                            className={classes.textField} value={this.state.email} onChange={this.handleChange}>
+                        </TextField>
+                        <TextField
+                            id="password" name="password" type="password" label="Password" fullWidth 
+                            helperText={errors.password} error={errors.password ? true : false}
+                            className={classes.textField} value={this.state.password} onChange={this.handleChange}>
+                        </TextField>
+                        {errors.general && (
+                            <Typography variant="body2" className={classes.customError}>
+                                {errors.general}
+                            </Typography>
+                        )}
+                        <Button type="submit" variant="contained" color="primary" className={classes.button} disabled={loading}>
+                            Login
+                            {loading && (
+                                <CircularProgress size={30} className={classes.progress} />
+                            )}
+                        </Button>
+                        <br/>
+                        <small>Dont't have an account? Sign up <Link to="/signup">here!</Link></small>
+                    </form>
+                </Grid>
+                <Grid item sm />
+            </Grid>
         )
     }
 }
 
-export default Login;
+Login.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Login);
