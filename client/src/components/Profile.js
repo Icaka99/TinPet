7d32158ec9294f63302962a5951f56526a1ef57b
Hristo Kisimov <hkisimov@sidigitalsolutions.com>
2021-12-22 15:42:51 +0200

    Implement logic for uploading a profile picture

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -6,74 +6,95 @@ import dayjs from 'dayjs';
 
 //Redux
 import { connect } from 'react-redux';
+import { logoutUser, uploadImage } from '../redux/actions/userActions';
 
 //MUI stuff
-import { Button, Paper } from '@material-ui/core';
+import { Button, IconButton, Paper } from '@material-ui/core';
 import MuiLink from '@material-ui/core/Link'
 import Typography from '@material-ui/core/Typography';
+import { Tooltip } from '@material-ui/core';
 
 //Icons
 import LocationOn from '@material-ui/icons/LocationOn';
 import LinkIcon from '@material-ui/icons/Link';
 import CalendarToday from '@material-ui/icons/CalendarToday';
+import EditIcon from '@material-ui/icons/Edit';
 
 const styles = (theme) => ({
     paper: {
-        padding: 20   
+        padding: 20
     },
     profile: {
         '& .image-wrapper': {
-          textAlign: 'center',
-          position: 'relative',
-          '& button': {
-            position: 'absolute',
-            top: '80%',
-            left: '70%'
-          }
+            textAlign: 'center',
+            position: 'relative',
+            '& button': {
+                position: 'absolute',
+                top: '80%',
+                left: '70%'
+            }
         },
         '& .profile-image': {
-          width: 200,
-          height: 200,
-          objectFit: 'cover',
-          maxWidth: '100%',
-          borderRadius: '50%'
+            width: 200,
+            height: 200,
+            objectFit: 'cover',
+            maxWidth: '100%',
+            borderRadius: '50%'
         },
         '& .profile-details': {
-          textAlign: 'center',
-          '& span, svg': {
-            verticalAlign: 'middle'
-          },
-          '& a': {
-            color: '#00bcd4'
-          }
+            textAlign: 'center',
+            '& span, svg': {
+                verticalAlign: 'middle'
+            },
+            '& a': {
+                color: '#00bcd4'
+            }
         },
         '& hr': {
-          border: 'none',
-          margin: '0 0 10px 0'
+            border: 'none',
+            margin: '0 0 10px 0'
         },
         '& svg.button': {
-          '&:hover': {
-            cursor: 'pointer'
-          }
+            '&:hover': {
+                cursor: 'pointer'
+            }
         }
-      },
-      buttons: {
+    },
+    buttons: {
         textAlign: 'center',
         '& a': {
-          margin: '20px 10px'
+            margin: '20px 10px'
         }
-      }
+    }
 });
 
 class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.imageInput= React.createRef();
+    }
+
+
+    handleImageChange = (event) => {
+        const image = event.target.files[0];
+        const formData = new FormData();
+        formData.append('image', image, image.name);
+        this.props.uploadImage(formData);
+    };
+
+    handleEditPicture = (event) => {
+        const fileInput = this.imageInput.current;
+        fileInput.click();
+    }
+
     render() {
-        const { 
+        const {
             classes,
-            user: { 
-                credentials: { handle, createdAt, imageUrl, bio, website, location }, 
+            user: {
+                credentials: { handle, createdAt, imageUrl, bio, website, location },
                 loading,
                 authenticated
-            } 
+            }
         } = this.props;
 
         let profileMarkup = !loading ? (authenticated ? (
@@ -82,19 +103,25 @@ class Profile extends Component {
                 <div className={classes.profile}>
                     <div className="image-wrapper">
                         <img src={imageUrl} alt="profile" className="profile-image" />
+                        <input type="file" id="imageInput" onChange={this.handleImageChange} ref={this.imageInput} hidden="hidden" />
+                        <Tooltip title="Edit profile picture">
+                            <IconButton onClick={this.handleEditPicture} className="button">
+                                <EditIcon color="primary" />
+                            </IconButton>
+                        </Tooltip>
                     </div>
-                    <hr/>
+                    <hr />
                     <div className="profile-details">
                         <MuiLink component={Link} to={`/users/${handle}`} color="primary" variant="h5">
                             @{handle}
                         </MuiLink>
-                        <hr/>
+                        <hr />
                         {bio && <Typography variant="body2">{bio}</Typography>}
-                        <hr/>
+                        <hr />
                         {location && (
                             <Fragment>
                                 <LocationOn color="primary"> <span>{location}</span></LocationOn>
-                                <hr/>
+                                <hr />
                             </Fragment>
                         )}
                         {website && (
@@ -104,7 +131,7 @@ class Profile extends Component {
                                     {' '}
                                     {website}
                                 </a>
-                                <hr/>
+                                <hr />
                             </Fragment>
                         )}
                         <CalendarToday color="primary" /> {' '}
@@ -137,9 +164,13 @@ const mapStateToProps = (state) => ({
     user: state.user
 });
 
+const mapActionsToProps = { logoutUser, uploadImage };
+
 Profile.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    uploadImage: PropTypes.func.isRequired,
     user: PropTypes.object.isRequired,
     classes: PropTypes.object.isRequired
 }
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile));
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile));
